9707cfd8dd1a7626df4cd0e88ad01fc463825bb6
Allen Shaji <allenshaji10@gmail.com>
2020-09-07 22:37:25 +0530

    new

---
+++ src/view/home.js
@@ -0,0 +1,13 @@
+import Moon from "moon";
+import moonLogo from "images/moon-logo.png";
+const { div, img, h1, p, a } = Moon.view.m;
+
+
+export default ({ data }) =>
+	<div id="root" class="a-x-c a-y-l-c">
+		<div class="s-x-25 a-x-c m-y-3">
+			<img src=moonLogo alt="Moon logo" class="s-x-15 s-y-15"/>
+			<h1 class="s-x-25 a-t-c">{data.name}</h1>
+			<p class="s-x-25 a-t-c"><a href="http://allenshaji.me" target="_blank">Allen Shaji</a></p>
+		</div>
+	</div>;
